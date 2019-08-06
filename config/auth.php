6717630d1f416adeb46c75760dcda723412217b8
HussamAdil <hussam0683@gmail.com>
2019-08-06 08:33:49 +0200

    install jtw

--- config/auth.php
+++ config/auth.php
@@ -14,10 +14,10 @@ return [
     */
 
     'defaults' => [
-        'guard' => 'web',
+        'guard' => 'api',
         'passwords' => 'users',
     ],
-
+    
     /*
     |--------------------------------------------------------------------------
     | Authentication Guards
@@ -42,7 +42,7 @@ return [
         ],
 
         'api' => [
-            'driver' => 'token',
+            'driver' => 'jwt',
             'provider' => 'users',
             'hash' => false,
         ],
--- routes/api.php
+++ routes/api.php
@@ -18,6 +18,7 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 });
 
 
+
 Route::apiResource('/question' , 'QuestionController');
 
 Route::apiResource('/category' , 'CategoryController');
@@ -27,3 +28,17 @@ Route::apiResource('/question/{question}/reply/' , 'ReplyController');
 Route::post('/like/{reply}/' , 'LikeController@likeIt');
 
 Route::Delete('/like/{reply}/' , 'LikeController@unLikeIt');
+
+Route::group([
+
+    'middleware' => 'api',
+    'prefix' => 'auth'
+
+], function ($router) {
+
+    Route::post('login', 'AuthController@login');
+    Route::post('logout', 'AuthController@logout');
+    Route::post('refresh', 'AuthController@refresh');
+    Route::post('me', 'AuthController@me');
+
+});
