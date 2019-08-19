240f86c0ba43235e1a00f77b8fcf27550a3f0bbb
HussamAdil <hussam0683@gmail.com>
2019-08-19 23:18:58 +0200

    create question backend part 1

--- public/js/app.js
+++ public/js/app.js
@@ -109371,6 +109371,8 @@ try {
 
 window.axios = __webpack_require__(/*! axios */ "./node_modules/axios/index.js");
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+var JtWToken = 'Bearer ' + localStorage.getItem('token');
+window.axios.defaults.headers.common['Authorization'] = JtWToken;
 /**
  * Next we will register the CSRF Token as a common header with Axios so that
  * all outgoing HTTP requests automatically have it attached. This is just
